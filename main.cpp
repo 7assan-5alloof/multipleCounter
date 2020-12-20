cc3f0f287b91f79065091567d38f93d07293d0f0
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-20 17:50:54 +0400

    Finish project

--- main.cpp
+++ main.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int smallest, greatest, target, diff, multiples;
+    char stop;
+    while(true)
+    {
+        cout << "Enter the smallest number of your range: ";
+        cin >> smallest;
+        cout << "Enter the greatest number of your range: ";
+        cin >> greatest;
+        cout << "How many multiples of _____ are there in the specified range? Enter what to fill in the gap: ";
+        cin >> target;
+
+        diff = greatest - smallest;
+        multiples = diff / target;
+
+        cout << "There are " << multiples << " multiples of " << target << " in range [" << smallest << ", " << greatest << "]" << endl;
+        cout << "[C]ontinue (Any other character for stop)? ";
+        cin >> stop;
+        if (stop == 'C')
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
     return 0;
 }
